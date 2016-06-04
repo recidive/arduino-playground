8bafb307e06f22db876d7c0d295d25120740d790
Henrique Recidive <recidive@gmail.com>
2016-06-04 12:39:06 -0300

    Refactored drawing to OLED stuff.

--- OLED-temp/OLED-temp.ino
+++ OLED-temp/OLED-temp.ino
@@ -1,3 +1,7 @@
+/**
+ * Read temperature from a BMP180 sensor and print it on a OLED
+ * 128x64 display.
+ */
 
 #include <U8glib.h>
 #include <Adafruit_BMP085.h>
@@ -8,8 +12,24 @@ U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NO_ACK);
 // The BMP180 sensor.
 Adafruit_BMP085 bmp;
 
+// Update display information.
+void draw(double temp) {
+  // Picture loop.
+  u8g.firstPage();  
+  do {
+    drawTemp(temp);
+  } while (u8g.nextPage());
+}
+
 // Draw temperature to the OLED display.
-void drawTemp(char* buf) {
+void drawTemp(double temp) {
+  // Create temp display buffer.
+  char buf[20];
+
+  // Fill in buffer for displaying.
+  dtostrf(temp, 6, 1, buf);
+
+  // Set font and draw the string.
   u8g.setFont(u8g_font_fub25);
   u8g.drawStr(0, 50, buf);
 }
@@ -35,17 +55,8 @@ void loop(void) {
   // Read temperature sensor.
   double temp = readTemp();
 
-  // Create temp display buffer.
-  char buf[20];
-
-  // Fill in buffer for displaying.
-  dtostrf(temp, 6, 1, buf);
-
-  // Picture loop.
-  u8g.firstPage();  
-  do {
-    drawTemp(buf);
-  } while (u8g.nextPage());
+  // Update display.
+  draw(temp);
   
   // Rebuild the picture after some delay.
   delay(1000);
