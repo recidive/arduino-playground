1a61329253919c469a52efe821fdb4a7f026b176
Henrique Recidive <recidive@gmail.com>
2016-06-04 12:04:35 -0300

    Refactored code to be more performant.

--- OLED-temp/OLED-temp.ino
+++ OLED-temp/OLED-temp.ino
@@ -2,29 +2,29 @@
 #include <U8glib.h>
 #include <Adafruit_BMP085.h>
 
-
-
-
 // The OLED display.
 U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NO_ACK);
 
 // The BMP180 sensor.
 Adafruit_BMP085 bmp;
 
-void drawTemp(void) {
+// Draw temperature to the OLED display.
+void drawTemp(char* buf) {
   u8g.setFont(u8g_font_fub25);
-
-  char buf[20];
-  double temp = bmp.readTemperature();
-
-  dtostrf(temp, 6, 1, buf);
-  
   u8g.drawStr(0, 50, buf);
 }
 
+// Read temperature sensor.
+double readTemp() {
+  // Read BMP180 sensor.
+  return bmp.readTemperature();
+}
+
 void setup(void) {
+  // Start serial log.
   Serial.begin(9600);
 
+  // Start BMP180 sensor.
   if (!bmp.begin()) {
     Serial.println("Could not find a valid BMP180 sensor, check wiring!");
     while (1) {}
@@ -32,10 +32,19 @@ void setup(void) {
 }
 
 void loop(void) {
+  // Read temperature sensor.
+  double temp = readTemp();
+
+  // Create temp display buffer.
+  char buf[20];
+
+  // Fill in buffer for displaying.
+  dtostrf(temp, 6, 1, buf);
+
   // Picture loop.
   u8g.firstPage();  
   do {
-    drawTemp();
+    drawTemp(buf);
   } while (u8g.nextPage());
   
   // Rebuild the picture after some delay.
